d97df275a980bfa08b8f421a97fbb20e17314777
Tobi Lehman <tobi.lehman@gmail.com>
2014-03-02 15:34:01 -0800

    clean up DeckTest

--- src/com/definidev/DeckTest.java
+++ src/com/definidev/DeckTest.java
@@ -7,14 +7,22 @@ public class DeckTest extends Assert {
 
 	@Test
 	public void testInitCount() {
-		Card cards[] = {new Card(Rank.ACE, Suit.CLUBS), new Card(Rank.TWO, Suit.CLUBS), new Card(Rank.THREE, Suit.HEARTS)};
+		Card cards[] = {
+				new Card(Rank.ACE, Suit.CLUBS), 
+				new Card(Rank.TWO, Suit.CLUBS), 
+				new Card(Rank.THREE, Suit.HEARTS)
+		};
 		Deck deck = new Deck(cards);
 		assertEquals(3, deck.count());
 	}
 	
 	@Test
 	public void testToString() {
-		Card cards[] = {new Card(Rank.ACE, Suit.CLUBS), new Card(Rank.TWO, Suit.CLUBS), new Card(Rank.THREE, Suit.HEARTS)};
+		Card cards[] = {
+				new Card(Rank.ACE, Suit.CLUBS), 
+				new Card(Rank.TWO, Suit.CLUBS),
+				new Card(Rank.THREE, Suit.HEARTS)
+		};
 		Deck deck = new Deck(cards);
 		assertEquals("AC 2C 3H", deck.toString());
 	}
@@ -23,7 +31,11 @@ public class DeckTest extends Assert {
 
 	@Test
 	public void testSortByRank() {
-		Card cards[] = {new Card(Rank.KING, Suit.CLUBS), new Card(Rank.FOUR, Suit.CLUBS), new Card(Rank.SIX, Suit.HEARTS)};
+		Card cards[] = {
+				new Card(Rank.KING, Suit.CLUBS), 
+				new Card(Rank.FOUR, Suit.CLUBS), 
+				new Card(Rank.SIX, Suit.HEARTS)
+		};
 		Deck deck = new Deck(cards);
 		deck.sortByRank();
 		
@@ -32,7 +44,11 @@ public class DeckTest extends Assert {
 
 	@Test
 	public void testSortBySuit() {
-		Card cards[] = {new Card(Rank.KING, Suit.DIAMONDS), new Card(Rank.FOUR, Suit.SPADES), new Card(Rank.SIX, Suit.HEARTS)};
+		Card cards[] = {
+				new Card(Rank.KING, Suit.DIAMONDS), 
+				new Card(Rank.FOUR, Suit.SPADES), 
+				new Card(Rank.SIX, Suit.HEARTS)
+		};
 		Deck deck = new Deck(cards);
 		deck.sortBySuit();
 		
@@ -44,7 +60,11 @@ public class DeckTest extends Assert {
 	
 	@Test
 	public void testGetAtIndex() {
-		Card cards[] = {new Card(Rank.KING, Suit.DIAMONDS), new Card(Rank.FIVE, Suit.SPADES), new Card(Rank.SIX, Suit.HEARTS)};
+		Card cards[] = {
+				new Card(Rank.KING, Suit.DIAMONDS), 
+				new Card(Rank.FIVE, Suit.SPADES), 
+				new Card(Rank.SIX, Suit.HEARTS)
+		};
 		Deck deck = new Deck(cards);
 		Card fourOfSpades = new Card(Rank.FIVE, Suit.SPADES);
 		
